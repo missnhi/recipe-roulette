fb878dd176a6b1dc06a2a8d029026a085ff6a486
missnhi <nhi.phan.ley@gmail.com>
2024-12-17 18:11:42 -0700

    Recipe's front page

--- app/page.js
+++ app/page.js
@@ -1,100 +1,92 @@
-import Image from "next/image";
+"use client";
+import "@/app/styles/navbar.css";
+import "@/app/styles/lottie.css";
+import "@/app/styles/header.css";
+import "@/app/styles/recipe-div.css"
+import "@/app/styles/footer.css"
+import { useState, useRef, useEffect } from "react";
 
 export default function Home() {
+  const [loading, setLoading] = useState(false);
+  const [showRecipe, setShowRecipe] = useState(false);
+  const lottieRef = useRef(null);
+  // useEffect(() => {
+  //   if (lottieRef.current) {
+  //     lottieRef.current.addEventListener('load', () => {
+  //       lottieRef.current.play();
+  //     });
+  //   }
+  // }, []);
+  
+  const onClickLoading = () => {
+    console.log('Should now show the recipe');
+    setLoading(true);
+    // if (lottieRef.current) {
+    //   lottieRef.current.play();
+    // }
+    setTimeout(() => {
+      setLoading(false);
+      setShowRecipe(true);
+    }, 2000); // 2 seconds loading time
+  };
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div>
+      <script
+        src="https://unpkg.com/@dotlottie/player-component@2.7.12/dist/dotlottie-player.mjs"
+        type="module"
+      ></script>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Exo+2:ital,wght@0,100..900;1,100..900&family=Lexend:wght@100..900&display=swap"
+        rel="stylesheet"
+      ></link>
+      <title>Recipe Roulette</title>
+      
+      <nav>
+        <div className="nav-left">
+          <a href="#home">Home</a>
+          <a href="#about">About</a>
+          <a href="#contact">Contact</a>
         </div>
+        <div className="nav-center lexend-font">RECIPE ROULETTE</div>
+        
+        <div className="nav-right">
+          <a href="#signin">Sign In</a>
+          <a href="#signup">Sign Up</a>
+        </div>
+      </nav>
+      <header>
+        
+        <div className="dotlottie-player-container">
+          <div> CLICK </div>
+          <button onClick={onClickLoading}>
+            <div ref={lottieRef}>
+              <dotlottie-player
+                src="https://lottie.host/cf4b572d-fada-4365-aa08-b3a0545cef4a/WtIaO5mAdH.lottie"
+                background="transparent"
+                speed="1"
+                style={{ width: "200px", height: "200px" }}
+                loop
+                autoplay={true}
+              ></dotlottie-player>
+            </div>
+          </button>
+          <div> TO ROLL </div>
+        </div>
+      
+      </header>
+      <main className="recipe-div">
+        {showRecipe && (<div>
+          This is where the recipe will be shown <br></br>
+          RECIPE!!!!!!!!!!<br></br>
+          RECIPE!!!!!!!!!!<br></br>
+          RECIPE!!!!!!!!!!<br></br>
+          RECIPE!!!!!!!!!!
+        </div>)}
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <footer>
+      
       </footer>
     </div>
   );

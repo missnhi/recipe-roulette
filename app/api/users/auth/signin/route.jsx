1bce7b425e53db0145387d75955e6dfb7e2d1476
anthonyChuks1 <anuebunwaanthony1@gmail.com>
2024-12-26 16:22:44 -0500

    removed global css and layout also implemented signin does not work yet

--- app/api/users/auth/signin/route.jsx
+++ app/api/users/auth/signin/route.jsx
@@ -1,32 +1,29 @@
+import { checkValidInfo } from "../../../../helpers/helpers";
 import { PrismaClient } from "@prisma/client";
-import { checkValidInfo } from "../../../../../helpers/helpers";
+import { cookies } from "next/headers";
 
 const prisma = new PrismaClient();
-export async function GET(req, res) {
-  const signInForm = ` <h1>Sign In</h1>
-  <form action="/api/users/auth/signin" method="POST">
-    <div>
-      <label for="email">Email:</label>
-      <input type="email" id="email" name="email" required>
-    </div>
-    <div>
-      <label for="password">Password:</label>
-      <input type="password" id="password" name="password" required>
-    </div>
-    <button type="submit">Sign In</button>
-  </form>`;
-  return new Response(signInForm, {
-    status: 200,
-    headers: {
-      "Content-Type": "text/html",
-    },
-  });
-}
 
 export async function POST(req, res) {
-  const body = await req.json();
-  const email = body.email;
-  const password = body.password;
+  let email, password;
+
+  if (req.headers.get("content-type") === "application/json") {
+    const body = await req.json();
+    email = body.email;
+    password = body.password;
+  } else if (req.headers.get("content-type") === "application/x-www-form-urlencoded") {
+    const formData = await req.formData();
+    email = formData.get("email");
+    password = formData.get("password");
+  } else {
+    return new Response(JSON.stringify({ message: "Unsupported content type" }), {
+      status: 400,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
   console.log(`Email: ${email} \n Password:  ${password}`);
 
   return checkValidInfo(email, password, prisma)
@@ -51,15 +48,15 @@ export async function POST(req, res) {
       }
     })
     .catch((error) => {
-      console.log("Error during login:", error);
-      return new Response(
-        JSON.stringify({ message: "Internal server error" }),
-        {
-          status: 500,
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
+      console.error("Error during login:", error);
+      return new Response(JSON.stringify({ message: "Internal server error" }), {
+        status: 500,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    })
+    .finally(() => {
+      prisma.$disconnect();
     });
 }

ca79ec8b89d5eb5f950e87a6ec01ed8d17d1dd52
missnhi <nhi.phan.ley@gmail.com>
2025-01-07 18:40:26 -0700

    Ingredient API

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    SPOONACULAR_API_KEY: process.env.SPOONACULAR_API_KEY,
+  },
+};
 
 export default nextConfig;
